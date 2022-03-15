439b7272a4771fda2a3b2dd6de99679b42fd8bf1
cmdrGuyson <50499511+cmdrGuyson@users.noreply.github.com>
2022-03-15 19:09:24 +0530

    changes

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Game from "../../components/game/game";
 
 import leaves from "../../assets/images/leaves.svg";
+import lines from "../../assets/images/lines.svg";
 
 import Info from "../../components/info/info";
 
@@ -10,6 +11,7 @@ import "./main.css";
 const Main = () => {
   return (
     <div className="main">
+      <img src={lines} className="lines-right" alt="lines-right" />
       <img src={leaves} className="leaves-left" alt="leaves-left"></img>
       <Game />
       <Info />
